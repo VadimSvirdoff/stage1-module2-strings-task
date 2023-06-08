28b06c1273eb9c3a6f785747b9d57cf4bafe9130
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-08 19:22:39 +0300

    My first commit!

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -2,6 +2,7 @@ package com.epam.mjc;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.ArrayList;
 
 public class StringSplitter {
 
@@ -13,6 +14,36 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> substrings = new ArrayList<>();
+
+        // Start index for substring extraction
+        int startIndex = 0;
+
+        // Iterate through the source string
+        for (int i = 0; i < source.length(); i++) {
+            // Check if any delimiter matches at the current index
+            for (String delimiter : delimiters) {
+                if (source.startsWith(delimiter, i)) {
+                    // Extract the substring from startIndex to i
+                    String substring = source.substring(startIndex, i);
+                    if (!substring.isEmpty()) {
+                        substrings.add(substring);
+                    }
+
+                    // Update the startIndex for the next substring extraction
+                    startIndex = i + delimiter.length();
+                    i = startIndex - 1; // Decrement i to recheck the current character
+                    break;
+                }
+            }
+        }
+
+        // Add the remaining substring after the last delimiter
+        String lastSubstring = source.substring(startIndex);
+        if (!lastSubstring.isEmpty()) {
+            substrings.add(lastSubstring);
+        }
+
+        return substrings;
     }
 }
